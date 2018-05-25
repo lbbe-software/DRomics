441384198888509b27ca744ee6543cb73e6ffdf2
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2018-05-25 16:33:40 +0200

    Add 'Aurelie Siberchicot' in the copyright section of the head of the R files

--- R/omicdata.R
+++ R/omicdata.R
@@ -1,5 +1,6 @@
 ######################################################################################
-#   Copyright (c) 2018 Marie Laure Delignette-Muller, Elise Billoir, Floriane Larras                                                                                                  
+#   Copyright (c) 2018 Marie Laure Delignette-Muller, Elise Billoir, Floriane Larras,
+#   Aurelie Siberchicot
 #                                                                                                                                                                        
 #   This program is free software; you can redistribute it and/or modify                                               
 #   it under the terms of the GNU General Public License as published by                                         
