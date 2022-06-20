9e1f3c7ef6986f74636bad985f6f32b3fe01e483
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2022-06-20 14:42:41 +0200

    Update the DRomicsML shiny app: in step4, update the layout with dose log transfo, min dose and max dose

--- inst/DRomicsMultiLevels-shiny/ui.R
+++ inst/DRomicsMultiLevels-shiny/ui.R
@@ -309,16 +309,13 @@ ui <- fluidPage(
                       wellPanel(
                         fixedRow(
                           column(2, 
-                                 numericInput("mindoseCurvesplot", label = "Minimal dose for the x range", value = 0),
-                                 numericInput("mindoseCurvesplot", label = "Maximal dose for the x range", value = 1)
-                          ),
-                          column(2, 
                                  splitLayout(cellWidths = c("70%", "30%"),
                                              checkboxInput("doselogtransfoCurvesplot", label = HTML("<b>Dose log transformation</b>"), value = FALSE),
-                                             checkboxInput("doselogtransfoCurvesplot", label = HTML("<b>Dose log transformation</b>"), value = FALSE),
-                                             # shinyBS::bsButton("helplabel1step4", label = "", icon = icon("info"), size = "small", style="color:#9c5c16"),
-                                             # shinyBS::bsPopover("helplabel1step4", "", helplabel1step4, placement = "right", trigger = "hover", options = NULL)
-                                 )
+                                             shinyBS::bsButton("helplabel1step4", label = "", icon = icon("info"), size = "small", style="color:#9c5c16"),
+                                             shinyBS::bsPopover("helplabel1step4", "", helplabel1step4, placement = "right", trigger = "hover", options = NULL)
+                                 ),
+                                 numericInput("mindoseCurvesplot", label = "Minimal dose for the x range", value = 0, width = "60%"),
+                                 numericInput("maxdoseCurvesplot", label = "Maximal dose for the x range", value = 1, width = "60%")
                           ),
                           column(2,
                                  radioButtons("facetbycolumnsCurvesplot", label = "Facet by (for columns)", 
