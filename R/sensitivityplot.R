5fd43ea72a9886c5f3b3330bec8c8d28d73de61e
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2022-07-19 15:24:34 +0200

    Clean and align the 'sensitivityplot' code to a better readability

--- R/sensitivityplot.R
+++ R/sensitivityplot.R
@@ -84,27 +84,29 @@ sensitivityplot <- function(extendedres, BMDtype = c("zSD", "xfold"),
     # fix the order of the modalities of by as in the ordered data set
     dnb$groupby <- factor(dnb$groupby, levels = dnb$groupby)
   }
+  
   if (BMDsummary == "first.quartile")
   {
     if (missing(colorby))
-    gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(firstquartile), 
-                           size = quote(nb_of_items))) else
-    gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(firstquartile), 
-                          color = quote(level), alpha = I(0.8),
-                          size = quote(nb_of_items))) 
-                                                      
+      gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(firstquartile), 
+                             size = quote(nb_of_items)))
+    else
+      gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(firstquartile), 
+                             color = quote(level), alpha = I(0.8),
+                             size = quote(nb_of_items))) 
+    
     gg <- gg + geom_point(stat = 'identity')  +  coord_flip() +
       labs(x = "", y = "BMD 25th quantiles") 
-        
+    
   } else {
     if (missing(colorby))
       gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(secondquartile), 
                              size = quote(nb_of_items))) 
     else {
       if (BMDsummary == "median") {
-      gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(secondquartile), 
-                             color = quote(level), 
-                             size = quote(nb_of_items)))
+        gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(secondquartile), 
+                               color = quote(level), 
+                               size = quote(nb_of_items)))
       } else {
         gg <- ggplot(dnb, aes_(x = quote(groupby), y = quote(secondquartile), 
                                color = quote(level), alpha = I(0.5),
@@ -115,12 +117,12 @@ sensitivityplot <- function(extendedres, BMDtype = c("zSD", "xfold"),
     
     if (BMDsummary == "median")
       gg <- gg + labs(x = "", y = "BMD medians") else
-      gg <- gg + geom_errorbar(aes_(ymin = quote(firstquartile), 
+        gg <- gg + geom_errorbar(aes_(ymin = quote(firstquartile), 
                                       ymax = quote(thirdquartile), size = I(1)), 
-                                      width = 0) +
-          # line to remove lines on the size legend
-          guides(size = guide_legend(override.aes = list(linetype = 0))) +
-          labs(x = "", y = "BMD medians and IQRs")
+                                 width = 0) +
+                # line to remove lines on the size legend
+                guides(size = guide_legend(override.aes = list(linetype = 0))) +
+                labs(x = "", y = "BMD medians and IQRs")
    }    
   if (BMD_log_transfo)
     gg <- gg + scale_y_log10()
