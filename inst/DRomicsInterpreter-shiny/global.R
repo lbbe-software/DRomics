b7578908807aa7fe3a4eaf1ac323ffc681d2cbd2
Marie-Laure DELIGNETTE-MULLER <mdeligne>
2022-09-20 14:54:49 +0200

    few modifs for importation of annotation data

--- inst/DRomicsInterpreter-shiny/global.R
+++ inst/DRomicsInterpreter-shiny/global.R
@@ -9,7 +9,7 @@ options(shiny.maxRequestSize=30*1024^2)
 
 texthelpnblevel <- "The maximal number of experimental levels is 10. The experimental levels can be for example different molecular levels (transcriptomics, metabolomis, ...), different experimental time points or different biological models (different species, different experimental settings), ..."
 texthelpmaxdosexscale <- "We recommand you to fix it at the maximal tested dose/concentration. If this value is not the same for all the experimental levels, keep the smallest one to prevent extrapolation of dose response curves in plots."
-helplabel1step1 <- "Each file for annotation data must have exactly two columns. Take care to reduce the dimension of your annotation file by keeping only the common items present in the DRomics output."
+helplabel1step1 <- "Each file for annotation data must have exactly two columns. Take care to reduce the dimension of your annotation file by keeping only the items which are present in the DRomics output and have at least one biological annotation."
 helplabel2step1 <- "If there are multiple experimental levels, the labels chosen must be unique. All spaces in the labels are removed."
 
 helplabel1step2 <- "To limit the number of annotation groups, you can use the thresholds on the number of items representing the group and/or the BMDsummary value of the group. You can also choose to keep the results of all the experimental levels as soon as the criteria are met for at least one experimental level."
