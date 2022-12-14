bc144350158b2a274cec65d079c62c5eb4ef8b21
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2022-12-14 15:44:50 +0100

    Update the hexsticker

--- inst/hexsticker/hexsticker.R
+++ inst/hexsticker/hexsticker.R
@@ -7,19 +7,16 @@ hexSticker::sticker(
   subplot  = here::here("inst", "hexsticker", "icon.png"),
   package  = "DRomics",
   filename = here::here("man", "figures", "hexsticker.png"),
-  dpi      = 600,
   
   p_size   = 0.0,         # Title
-  u_size   = 6.0,         # URL
-  p_family = "Aller_Rg",
+  u_size   = 0.0,         # URL
   
-  h_fill   = "#d0d0d0",   # Background
+  h_fill   = "#dedede",   # Background
   h_color  = "#9c5c16",   # Border
-  u_color  = "#433625",   # URL
   
   s_x      = 1.00,        # Subplot
   s_y      = 1.05,        # Subplot
   s_width  = 0.85,        # Subplot
-  
-  url      = "https://aursiber.github.io/DRomics"
+  s_height = 0.85,        # Subplot
+  asp = 1
 )
