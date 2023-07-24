0541e71a129627581bf3387444a038947a3636e8
Marie-Laure DELIGNETTE-MULLER <mdeligne>
2023-07-24 15:14:55 +0200

    correction of testthat tests

--- tests/testthat/test_itemselect.R
+++ tests/testthat/test_itemselect.R
@@ -3,6 +3,7 @@ test_that("itemselect works as expected on the number of selected probes",
   {
     skip_on_cran()
     skip_on_os(c("mac", "linux", "solaris"))
+    
     datafilename <- system.file("extdata", "transcripto_sample.txt", package="DRomics")
     (o <- microarraydata(datafilename, check = TRUE, norm.method = "cyclicloess"))
     (s_quad0p001 <- itemselect(o, select.method = "quadratic", FDR = 0.001))
