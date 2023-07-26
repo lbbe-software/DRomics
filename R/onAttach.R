4b31637757a345d38ddc8123b0246e1edaf9feea
Marie-Laure DELIGNETTE-MULLER <mdeligne>
2023-07-26 17:16:23 +0200

    reformating of the start message to make it more visible

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,9 +1,13 @@
 .onAttach <- function(libname, pkgname)
 {
     packageStartupMessage("\nDRomics has been loaded.")
-    packageStartupMessage("IMPORTANT CHANGES IN DEFAULT PLOT ARGUMENTS: Now all the plot functions use by default a log10 scale")
-    packageStartupMessage("for dose and BMD values, except curvesplot(), for which the use of a dose log scale requires the specification")
-    packageStartupMessage("by the user of a non null minimal value (xmin). We also put the default value of the argument scaling at TRUE in")
-    packageStartupMessage("curvesplot() and bmdplotwithgradient(), to focus on shapes of dose-responses rather than on their amplitude.\n")
+    packageStartupMessage("!!!! IMPORTANT CHANGES IN DEFAULT PLOT ARGUMENTS !!!!")
+    packageStartupMessage("Now all the plot functions use by default a log10 scale")
+    packageStartupMessage("for dose and BMD values, except curvesplot(),")
+    packageStartupMessage("for which the use of a dose log scale requires the specification")
+    packageStartupMessage("by the user of a non null minimal value (xmin).")
+    packageStartupMessage("We also put the default value of the argument scaling at TRUE")
+    packageStartupMessage("in curvesplot() and bmdplotwithgradient(),")
+    packageStartupMessage("to focus on shapes of dose-responses rather than on their amplitude.\n")
 }
   
