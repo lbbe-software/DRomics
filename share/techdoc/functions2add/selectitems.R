bbecc40e86b75f0f6203d7599a71d9a2ad161c82
Marie-Laure DELIGNETTE-MULLER <mdeligne>
2023-11-16 11:48:21 +0100

    work on selectitems

--- share/techdoc/functions2add/selectitems.R
+++ share/techdoc/functions2add/selectitems.R
@@ -1,6 +1,6 @@
 selectitems <- function(extendedres,
                         BMDtype = c("zSD", "xfold"),
-                        BMDfilter = c("definedBMD", "definedCI", "finiteCI") 
+                        BMDfilter = c("definedCI", "finiteCI", "definedBMD") 
                          )
 {
   if (missing(extendedres) | !is.data.frame(extendedres))
@@ -8,47 +8,53 @@ selectitems <- function(extendedres,
     (see ?selectitems for details).")
   
   BMDtype <- match.arg(BMDtype, c("zSD", "xfold"))
+  BMDfilter <- match.arg(BMDfilter, c("definedCI", "finiteCI", "definedBMD"))
   cnames <- colnames(extendedres)
   
+  # Definition of the filter to apply 
   if (BMDtype == "zSD")
   {  
     if (any(!is.element(c("BMD.zSD"), cnames)))
       stop("The first argument of selectitems must be a dataframe
       containing at least columns named id and BMD.zSD.")
-
-    # Elimination of rows with NA values 
-    extendedreswithoutNA <- extendedres[!is.na(extendedres$BMD.zSD),]
-    variable <- extendedreswithoutNA$BMD.zSD
-  } else 
+    BMD <- extendedres$BMD.zSD
+    if ((BMDfilter ==  "definedCI") | (BMDfilter ==  "finiteCI"))
+    {
+      if (any(!is.element(c("BMD.zSD.upper", "BMD.zSD.lower"), cnames)) )
+      stop("To apply a filter on BMD.zSD confidence intervals, the first argument of selectitems 
+      must be a dataframe containing at least columns named id and BMD.zSD, BMD.zSD.lower, BMD.zSD.upper.")
+      BMDupper <- extendedres$BMD.zSD.upper
+      BMDlower <- extendedres$BMD.zSD.lower
+    }
+  } else #so if (BMDtype == "xfold")
   {
     if (any(!is.element(c("BMD.xfold"), cnames)))
       stop("The first argument of selectitems must be a dataframe
       containing at least columns named id and BMD.xfold.")
-
-    # Elimination of rows with NA values 
-    extendedreswithoutNA <- extendedres[!is.na(extendedres$BMD.xfold),]
-    variable <- extendedreswithoutNA$BMD.xfold
+    BMD <- extendedres$BMD.xfold
+    if ((BMDfilter ==  "definedCI") | (BMDfilter ==  "finiteCI"))
+    {
+      if (any(!is.element(c("BMD.xfold.upper","BMD.xfold.lower"), cnames)))
+        stop("To apply a filter on BMD.xfold confidence intervals, the first argument of selectitems 
+      must be a dataframe containing at least columns named id and BMD.xfold, BMD.xfold.lower, BMD.xfold.upper.")
+      BMDupper <- extendedres$BMD.xfold.upper
+      BMDlower <- extendedres$BMD.xfold.lower
+    }
   }
-
-  # if (!missing(BMDmax))
-  # {
-  #   if (!is.numeric(BMDmax) | (BMDmax < 0))
-  #     stop("Wrong argument 'BMDmax'. If not omitted it must be a positive number.")
-  #   g_e2keep <- dnb[(dnb$nb_of_items >= nitemsmin) & (dnb$BMDsummary <= BMDmax), "group_explev"]
-  # } else
-  # {
-  #   g_e2keep <- dnb[dnb$nb_of_items >= nitemsmin, "group_explev"]
-  # }
-  # 
-  # if (keepallexplev & !missing(explev))
-  # {
-  #   g2keep <- unique(group_allexplev[group_explev %in% g_e2keep])
-  #   subextendedres <- extendedreswithoutNA[group_allexplev %in% g2keep, ]
-  # } else
-  # {
-  #   subextendedres <- extendedreswithoutNA[group_explev %in% g_e2keep, ]
-  # }
-
+  
+  # Filtering
+  if (BMDfilter == "definedCI")
+  {
+    subextendedres <- extendedres[!is.na(BMD) & !is.na(BMDupper) & !is.na(BMDlower), ]
+  } else
+    if (BMDfilter == "finiteCI")
+    {
+      subextendedres <- extendedres[is.finite(BMD) & is.finite(BMDupper) & is.finite(BMDlower), ]
+    } else
+      if (BMDfilter == "definedBMD")
+      {
+        subextendedres <- extendedres[!is.na(BMD), ]
+      } 
   return(subextendedres)
 }
 
@@ -67,12 +73,40 @@ s_quad <- itemselect(o, select.method = "quadratic", FDR = 0.05)
 f <- drcfit(s_quad, progressbar = TRUE)
 r <- bmdcalc(f)
 set.seed(1234) # to get reproducible results with a so small number of iterations
-(b <- bmdboot(r, niter = 50)) # with a non reasonable value for niter
+(b <- bmdboot(r, niter = 10)) # with a non reasonable value for niter
 # !!!! TO GET CORRECT RESULTS
 # !!!! niter SHOULD BE FIXED FAR LARGER , e.g. to 1000 
 # !!!! but the run will be longer 
 
-# Trial on BMD.zSD  ??
+### Examples on BMD.zSD (with no uncalculated BMD.zSD values)
+# plot of BMDs before filtering
+bmdplot(b$res, BMDtype = "zSD", point.size = 3, add.CI = TRUE)
+# keeping all items with defined BMD point estimate
+subres <- selectitems(b$res, BMDtype = "zSD", BMDfilter = "definedBMD")
+bmdplot(subres, BMDtype = "zSD", point.size = 3, add.CI = TRUE)
+# keeping all items with defined BMD point estimate and CI bounds
+subres <- selectitems(b$res, BMDtype = "zSD", BMDfilter = "definedCI")
+bmdplot(subres, BMDtype = "zSD", point.size = 3, add.CI = TRUE)
+# keeping all items with finite BMD point estimate and CI bounds
+subres <- selectitems(b$res, BMDtype = "zSD", BMDfilter = "finiteCI") 
+bmdplot(subres, BMDtype = "zSD", point.size = 3, add.CI = TRUE)
+
+### Examples on BMD.xfold (with some uncalculated BMD.xfold values)
+# plot of BMDs before filtering
+bmdplot(b$res, BMDtype = "xfold", point.size = 3, add.CI = TRUE)
+# keeping all items with defined BMD point estimate
+subres <- selectitems(b$res, BMDtype = "xfold", BMDfilter = "definedBMD")
+bmdplot(subres, BMDtype = "xfold", point.size = 3, add.CI = TRUE)
+# keeping all items with defined BMD point estimate and CI bounds
+subres <- selectitems(b$res, BMDtype = "xfold", BMDfilter = "definedCI")
+bmdplot(subres, BMDtype = "xfold", point.size = 3, add.CI = TRUE)
+# keeping all items with finite BMD point estimate and CI bounds
+subres <- selectitems(b$res, BMDtype = "xfold", BMDfilter = "finiteCI") 
+bmdplot(subres, BMDtype = "xfold", point.size = 3, add.CI = TRUE)
+ ##########################################################
+
+# Trial on BMD.zSD (ex. with no uncalculated BMD.zSD values)
+
 (BMD <- b$res$BMD.zSD)
 (BMDupper <- b$res$BMD.zSD.upper)
 (BMDlower <- b$res$BMD.zSD.lower)
